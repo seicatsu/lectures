768f8da765eb3a66d5ebc37ae1ef400a52c681b8
Emre Can Mert <69457117+seicatsu@users.noreply.github.com>
2022-11-12 12:13:31 +0300

    Create pratikornek.py

---
+++ hafta2/pratikornek.py
@@ -0,0 +1,24 @@
+# Örnek 1
+# İki basamaklı bir sayı girdiğinizde basamak değerlerini toplayan bir kod yazınız
+# Sayı giriniz:53
+# Örnek 53 girdiğinizde 5 + 3 -> 8 olarak çıktı vermesini istiyoruz
+
+# Örnek 2
+# Boy ve kilo girdiginizde BMI hesaplayan bir kod yazınız
+# BMI kiloyu, boy uzunluğunun karesine bölerek elde ediyoruz
+
+# Örnek 3
+# Kullanıcı yaşını girdiğinde kaç gün, kaç hafta, kaç ay yaşadığını söyleyen kod
+# Örneğin 20 yaşındaki biri girdiğinde "240 ay, 1040 hafta, 7300 gün yaşadınız."
+# olarak çıktı alalım.
+
+# Örnek 4
+# Kullanıcının haftada kaç saat çalıştığını ve saat başına kaç maaş aldığını
+# girmesi ile ayda ne kadar maaş aldığını sağlayan bir program yazalım
+
+# Örnek 5
+# Bir restorana gittiğinizde kaç kişi olduğunuzu ve toplam hesabın ne kadar
+# tuttuğunu söylediğiniz bir kod yazalım ve bunun sonucunda hesabın %12'si kadar
+# bahşişi hesaba ilave edip herkesin kişi başına ödemesi gereken parayı hesaplayan
+# bir kod yazalım
+# 5 kişi gittiniz ve 100 tl hesap geldi diyelim bu durumda çıktımız 22.40 olmalı
